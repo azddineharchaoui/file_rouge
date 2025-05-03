0705d6f998fced0cc6ccb89410cf5cb72b3582e7
S-al1 <salmaaitabbou972@gmail.comi>
2025-05-03 18:22:55 +0100

    adding the google map for site location

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -141,8 +141,7 @@
                     
                     <!-- Carte ou image de localisation -->
                     <div class="mt-8 rounded-lg overflow-hidden shadow">
-                        <img src="{{ asset('images/map.jpg') }}" alt="Notre localisation" class="w-full h-64 object-cover">
-                    </div>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20995.766129713436!2d2.269807971267557!3d48.86830153443837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66fc518637631%3A0x7c6b92d2c2465999!2zQ2hhbXBzLcOJbHlzw6llcywgUGFyaXMsIEZyYW5jZQ!5e0!3m2!1sen!2sma!4v1746292870861!5m2!1sen!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>                    </div>
                 </div>
             </div>
         </div>
